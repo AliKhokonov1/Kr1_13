0024a21ca78a1d7ea791114d5690dd7b339c1a55
AliKhokonov1 <alkh2002@mail.ru>
2020-10-01 15:45:57 +0300

    My program is there

--- Autotest.cpp
+++ Autotest.cpp
@@ -4,12 +4,20 @@
 #include <iostream>
 using namespace std;
 #define N 3
-double Autotest (void) {
+int Autotest (void) {
     CComplexPoint sk;
     CComplexVector q,e,sum,raz;
     ifstream fin("test.txt");
+    if (fin.fail()) {
+        cout<<"File did not open!"<<endl;
+        return 2;
+    }
     q.setVector(fin);e.setVector(fin);
     ifstream vin("restest.txt");
+    if (vin.fail()) {
+        cout<<"File did not open!"<<endl;
+        return 3;
+    }
     sum.setVector(vin);raz.setVector(vin);
     sk=e.scala(q);
     for (int i=0;i<N;i++) {
--- main.cpp
+++ main.cpp
@@ -10,10 +10,14 @@ int main () {
 Autotest();
 CComplexVector q,e;
 ifstream fin("zhozhozho.txt");
+if (fin.fail()) {
+        cout<<"File did not open!"<<endl;
+        return 1;
+    }
 q.setVector(fin);
 e.setVector(fin);
-cout<<"Скалярное произведение q и e: "<<endl; e.scala(q).print(); cout<<endl<<endl;
-cout<<"Сумма q и e: "<<endl; (q+e).printVector(); cout<<endl<<endl;
-cout<<"Разность q и e: "<<endl; (q-e).printVector();
+cout<<"scalar product q and e: "<<endl; e.scala(q).print(); cout<<endl<<endl;
+cout<<"sum q and e: "<<endl; (q+e).printVector(); cout<<endl<<endl;
+cout<<"difference q and e: "<<endl; (q-e).printVector();
 return 0;
 }
